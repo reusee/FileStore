564d8de7405aa29c1ecc9fc0147b1d5186e62f41
reus <reusee@ymail.com>
2013-11-04 23:27:38 +0800

    main: add list command

--- list.go
+++ list.go
@@ -1,4 +1,49 @@
 package main
 
+import (
+	"./utils"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+)
+
 func (self *App) runList() {
+	if len(self.snapshotSet.Snapshots) == 0 {
+		fmt.Printf("no snapshot\n")
+		os.Exit(0)
+	}
+	lastSnapshot := self.snapshotSet.Snapshots[len(self.snapshotSet.Snapshots)-1]
+
+	paths := make([]string, 0, len(lastSnapshot.Files))
+	for path := range lastSnapshot.Files {
+		paths = append(paths, path)
+	}
+	sort.Strings(paths)
+
+	b, err := self.getBaiduBackend()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var totalSize int64
+	for _, path := range paths {
+		file := lastSnapshot.Files[path]
+		complete := true
+		for _, chunk := range file.Chunks {
+			e, err := b.Exists(int(chunk.Length), chunk.Hash)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if !e {
+				complete = false
+			}
+		}
+		if complete {
+			fmt.Printf("%s\n", path)
+			totalSize += file.Size
+		}
+	}
+
+	fmt.Printf("%s\n", utils.FormatSize(int(totalSize)))
 }
